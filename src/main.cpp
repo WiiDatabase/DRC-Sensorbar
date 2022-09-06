e688dbb246ed967254595541af1b280fa0f64704
Andreas Bielawski <brawl@nyanya.de>
2022-09-06 18:31:51 +0200

    Add CI

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,11 @@
-#include <wups.h>
 #include <vpad/input.h>
+#include <wups.h>
 
 WUPS_PLUGIN_NAME("DRC-Sensorbar");
-WUPS_PLUGIN_DESCRIPTION("Enables the sensorbar of the Wii U GamePad permanently.");
+WUPS_PLUGIN_DESCRIPTION(
+    "Enables the sensorbar of the Wii U GamePad permanently.");
 WUPS_PLUGIN_VERSION("v2.0");
 WUPS_PLUGIN_AUTHOR("WiiDatabase.de");
 WUPS_PLUGIN_LICENSE("MIT");
 
-ON_APPLICATION_START() {
-    VPADSetSensorBar(VPAD_CHAN_0, true);
-} 
-
+ON_APPLICATION_START() { VPADSetSensorBar(VPAD_CHAN_0, true); }
